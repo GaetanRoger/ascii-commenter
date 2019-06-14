1a656b3c284984fa1877a73165eb6808a14c19ad
Gaetan Roger <gaetanroger.fr@gmail.com>
2019-06-15 00:41:16 +0200

    Updated to Angular 8

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,41 +1,35 @@
-import {async, TestBed} from '@angular/core/testing';
-import {AppComponent} from './app.component';
-import {MatButtonModule, MatCardModule, MatCheckboxModule, MatInputModule, MatSelectModule, MatToolbarModule} from '@angular/material';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {FormsModule} from '@angular/forms';
-import {ParametersInputComponent} from './components/parameters-input/parameters-input.component';
-import {FigletComponent} from './components/figlet/figlet.component';
-import {ClipboardModule, ClipboardService} from 'ngx-clipboard';
-import {HeaderComponent} from './components/header/header.component';
-import {BottomInfoComponent} from './components/bottom-info/bottom-info.component';
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-    beforeEach(async(() => {
-        TestBed.configureTestingModule({
-            imports: [
-                ClipboardModule,
-                FormsModule,
-                BrowserAnimationsModule,
-                MatSelectModule,
-                MatInputModule,
-                MatCardModule,
-                MatButtonModule,
-                MatCheckboxModule,
-                MatToolbarModule
-            ],
-            declarations: [
-                AppComponent,
-                ParametersInputComponent,
-                FigletComponent,
-                HeaderComponent,
-                BottomInfoComponent
-            ],
-            providers: [ClipboardService]
-        }).compileComponents();
-    }));
-    it('should create the app', async(() => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.debugElement.componentInstance;
-        expect(app).toBeTruthy();
-    }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'ascii-commenter'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('ascii-commenter');
+  });
+
+  it('should render title in a h1 tag', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ascii-commenter!');
+  });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,39 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-
-import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCardModule, MatCheckboxModule, MatInputModule, MatSelectModule, MatToolbarModule} from '@angular/material';
-import {FigletComponent} from './components/figlet/figlet.component';
-import {FormsModule} from '@angular/forms';
-import {ParametersInputComponent} from './components/parameters-input/parameters-input.component';
-import {ClipboardModule} from 'ngx-clipboard';
-import {HeaderComponent} from './components/header/header.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { ClipboardModule } from 'ngx-clipboard';
+import { MatButtonModule, MatCardModule, MatCheckboxModule, MatInputModule, MatSelectModule, MatToolbarModule } from '@angular/material';
+import { FigletComponent } from './components/figlet/figlet.component';
 import { BottomInfoComponent } from './components/bottom-info/bottom-info.component';
-
+import { HeaderComponent } from './components/header/header.component';
+import { ParametersInputComponent } from './components/parameters-input/parameters-input.component';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        FigletComponent,
-        ParametersInputComponent,
-        HeaderComponent,
-        BottomInfoComponent
-    ],
-    imports: [
-        BrowserModule,
-        ClipboardModule,
-        FormsModule,
-        BrowserAnimationsModule,
-        MatSelectModule,
-        MatInputModule,
-        MatCardModule,
-        MatButtonModule,
-        MatCheckboxModule,
-        MatToolbarModule
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    FigletComponent,
+    ParametersInputComponent,
+    HeaderComponent,
+    BottomInfoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ClipboardModule,
+    FormsModule,
+    MatSelectModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatToolbarModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
