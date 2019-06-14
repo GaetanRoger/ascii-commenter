e2da9522a4ff969b4ca8a08b6531780a793a028f
Gaetan Roger <gaetanroger.fr@gmail.com>
2019-06-15 01:18:46 +0200

    VB support

--- src/comments.ts
+++ src/comments.ts
@@ -1,36 +1,40 @@
-import {CommentType} from './app/services/comments/comment-type';
+import { CommentType } from './app/services/comments/comment-type';
 
 const comments: CommentType[] = [
-    {
-        name: 'None',
-        char: '',
-        space: false
-    },
-    {
-        name: 'C',
-        char: '*',
-        first: '/*',
-        last: ' */',
-        left: ' *'
-    },
-    {
-        name: 'C++',
-        char: '/',
-        left: '//',
-        right: '//'
-    },
-    {
-        name: 'Python',
-        char: '#'
-    },
-    {
-        name: 'HTML',
-        char: '-',
-        first: '<!--',
-        last: '  -->',
-        left: '  --',
-        right: '--'
-    }
+  {
+    name: 'None',
+    char: '',
+    space: false
+  },
+  {
+    name: 'C',
+    char: '*',
+    first: '/*',
+    last: ' */',
+    left: ' *'
+  },
+  {
+    name: 'C++',
+    char: '/',
+    left: '//',
+    right: '//'
+  },
+  {
+    name: 'Python',
+    char: '#'
+  },
+  {
+    name: 'HTML',
+    char: '-',
+    first: '<!--',
+    last: '  -->',
+    left: '  --',
+    right: '--'
+  },
+  {
+    name: 'Visual Basic',
+    char: '\''
+  }
 ];
 
 export default comments;
