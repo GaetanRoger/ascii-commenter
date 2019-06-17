dad7815606ea4db9712ee522f21e4912c1a4e6e0
Gaetan Roger <gaetanroger.fr@gmail.com>
2019-06-17 20:47:19 +0200

    Fixed karma config to serve assets

--- src/app/services/figlet/figlet.service.ts
+++ src/app/services/figlet/figlet.service.ts
@@ -1,45 +1,46 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Fonts, text as figlet, defaults } from 'figlet';
+import { defaults, Fonts, text as figlet } from 'figlet';
 
 @Injectable()
 export class FigletService {
 
-    constructor() {
-        defaults({fontPath: 'assets/fonts'});
-    }
+  constructor() {
+    defaults({fontPath: '/assets/fonts'});
+  }
 
-    /**
-     * Return the text in the figlet font.
-     *
-     * If the text is undefined, null or empty, returns an empty string.
-     */
-    text(text: string, font: Fonts): Observable<string> {
-        return new Observable<string>(subscriber => {
-            // If text is undefined, null or empty, return an empty string
-            if (!text) {
-                subscriber.next('');
-                subscriber.complete();
-                return;
-            }
+  /**
+   * Return the text in the figlet font.
+   *
+   * If the text is undefined, null or empty, returns an empty string.
+   */
+  text(text: string, font: Fonts): Observable<string> {
+    return new Observable<string>(subscriber => {
+      // If text is undefined, null or empty, return an empty string
+      if (!text) {
+        subscriber.next('');
+        subscriber.complete();
+        return;
+      }
 
+      figlet(text, font, (err: object, res: string) => {
+        console.log('err', err, 'res', res);
 
-            figlet(text, font, (err: object, res: string) => {
-                let lines = res.split('\n');
+        let lines = res.split('\n');
 
-                // Removing empty lines
-                lines = lines.filter(l => l.trim().length > 0);
+        // Removing empty lines
+        lines = lines.filter(l => l.trim().length > 0);
 
-                // Removing blanks characters at the beginning of every line
-                while (lines.every(v => v.charAt(0) === ' ')) {
-                    lines = lines.map(l => l.substr(1));
-                }
+        // Removing blanks characters at the beginning of every line
+        while (lines.every(v => v.charAt(0) === ' ')) {
+          lines = lines.map(l => l.substr(1));
+        }
 
-                subscriber.next(lines.join('\n'));
-                subscriber.complete();
-            });
-        });
+        subscriber.next(lines.join('\n'));
+        subscriber.complete();
+      });
+    });
 
-    }
+  }
 
 }
