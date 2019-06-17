a23273572cc042e00a502e26b33a34f00cef375c
Gaetan Roger <gaetanroger.fr@gmail.com>
2019-06-17 21:14:40 +0200

    Cleaned figlet components

--- src/app/components/figlet/figlet.component.ts
+++ src/app/components/figlet/figlet.component.ts
@@ -1,75 +1,80 @@
-import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
-import {FigletService} from '../../services/figlet/figlet.service';
-import {ClipboardService} from 'ngx-clipboard';
-import {CommentsService} from '../../services/comments/comments.service';
-import {BorderService} from '../../services/border/border.service';
-import {MarginsService} from '../../services/margins/margins.service';
-import {Margins} from '../../services/margins/margins';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { FigletService } from '../../services/figlet/figlet.service';
+import { ClipboardService } from 'ngx-clipboard';
+import { CommentsService } from '../../services/comments/comments.service';
+import { BorderService } from '../../services/border/border.service';
+import { MarginsService } from '../../services/margins/margins.service';
+import { Margins } from '../../services/margins/margins';
 import { Fonts } from 'figlet';
+import { map, take } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
-    selector: 'app-figlet',
-    templateUrl: './figlet.component.html',
-    styleUrls: ['./figlet.component.css'],
-    providers: [
-        FigletService,
-        MarginsService,
-        CommentsService,
-        BorderService,
-        ClipboardService
-    ]
+  selector: 'app-figlet',
+  templateUrl: './figlet.component.html',
+  styleUrls: ['./figlet.component.css'],
+  providers: [
+    FigletService,
+    MarginsService,
+    CommentsService,
+    BorderService,
+    ClipboardService
+  ]
 })
 export class FigletComponent implements OnInit, OnChanges {
-    @Input()
-    text: string;
+  @Input()
+  text: string;
 
-    @Input()
-    font: Fonts;
+  @Input()
+  font: Fonts;
 
-    @Input()
-    comment = 'None';
+  @Input()
+  comment = 'None';
 
-    @Input()
-    bodered = false;
+  @Input()
+  bordered = false;
 
-    @Input()
-    margins: Margins = {};
+  @Input()
+  margins: Margins = {};
 
-    figlet: string;
-    copied = false;
+  figlet$: Observable<string>;
 
-    constructor(private readonly figletService: FigletService,
-                private readonly marginsService: MarginsService,
-                private readonly commentsService: CommentsService,
-                private readonly borderService: BorderService,
-                private readonly clipboardService: ClipboardService) {
-    }
+  copied = false;
 
-    ngOnInit() {
-        this.update();
-    }
+  constructor(private readonly figletService: FigletService,
+              private readonly marginsService: MarginsService,
+              private readonly commentsService: CommentsService,
+              private readonly borderService: BorderService,
+              private readonly clipboardService: ClipboardService) {
+  }
 
-    ngOnChanges(changes: SimpleChanges): void {
-        this.update();
-    }
+  ngOnInit() {
+    this.update();
+  }
 
-    private update() {
-        this.figletService.text(this.text, this.font).subscribe(
-            v => {
-                v = this.marginsService.add_margins(v, this.margins);
-                v = this.commentsService.comment(v, this.comment);
-                v = this.bodered ? this.borderService.border(v, this.comment) : v;
-                this.figlet = v;
-            }
-        );
-    }
+  ngOnChanges(changes: SimpleChanges): void {
+    this.update();
+  }
 
-    copyToClipboard() {
+  copyToClipboard() {
+    this.figlet$
+      .pipe(take(1))
+      .subscribe(f => {
+        this.clipboardService.copyFromContent(f);
         this.copied = true;
-        this.clipboardService.copyFromContent(this.figlet);
 
         setTimeout(() => {
-            this.copied = false;
+          this.copied = false;
         }, 2000);
-    }
+      });
+  }
+
+  private update() {
+    this.figlet$ = this.figletService.text(this.text, this.font)
+      .pipe(
+        map(t => this.marginsService.add_margins(t, this.margins)),
+        map(t => this.commentsService.comment(t, this.comment)),
+        map(t => this.bordered ? this.borderService.border(t, this.comment) : t)
+      );
+  }
 }
